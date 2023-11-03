af46518ff15d9ba5ad9d47ad97de4645b023f14c
Mitsuhiro <mitsueiwa333@gmail.com>
2023-11-03 15:22:13 -0300

    first commit

---
+++ config/tablar.php
@@ -0,0 +1,211 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Title
+    |--------------------------------------------------------------------------
+    | Here you can change the default title of your admin panel.
+    |
+    */
+
+    'title' => 'Tablar',
+    'title_prefix' => '',
+    'title_postfix' => '',
+    'bottom_title' => 'Tablar',
+    'current_version' => 'v4.0',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Panel Logo
+    |--------------------------------------------------------------------------
+    |
+    | Here you can change the logo of your admin panel.
+    |
+    */
+
+    'logo' => '<b>Tab</b>LAR',
+    'logo_img_alt' => 'Admin Logo',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Logo
+    |--------------------------------------------------------------------------
+    |
+    | Here you can set up an alternative logo to use on your login and register
+    | screens. When disabled, the admin panel logo will be used instead.
+    |
+    */
+
+    'auth_logo' => [
+        'enabled' => false,
+        'img' => [
+            'path' => 'assets/tablar-logo.png',
+            'alt' => 'Auth Logo',
+            'class' => '',
+            'width' => 50,
+            'height' => 50,
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Layout
+    |--------------------------------------------------------------------------
+    | Here we change the layout of your admin panel.
+    |
+    | For detailed instructions you can look at the layout section here:
+    |
+    */
+
+    'layout' => 'horizontal',
+    //boxed, combo, condensed, fluid, fluid-vertical, horizontal, navbar-overlap, navbar-sticky, rtl, vertical, vertical-right, vertical-transparent
+
+    'layout_light_sidebar' => null,
+    'layout_light_topbar' => true,
+    'layout_enable_top_header' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Panel Classes
+    |--------------------------------------------------------------------------
+    |
+    | Here you can change the look and behavior of the admin panel.
+    |
+    | For detailed instructions, you can look at the admin panel classes here:
+    |
+    */
+
+    'classes_body' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | URLs
+    |--------------------------------------------------------------------------
+    |
+    | Here we can modify the url settings of the admin panel.
+    |
+    | For detailed instructions, you can look at the urls section here:
+    |
+    */
+
+    'use_route_url' => true,
+    'dashboard_url' => 'home',
+    'logout_url' => 'logout',
+    'login_url' => 'login',
+    'register_url' => 'register',
+    'password_reset_url' => 'password.request',
+    'password_email_url' => 'password.email',
+    'profile_url' => false,
+    'setting_url' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Display Alert
+    |--------------------------------------------------------------------------
+    |
+    | Display Alert Visibility.
+    |
+    */
+    'display_alert' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Menu Items
+    |--------------------------------------------------------------------------
+    |
+    | Here we can modify the sidebar/top navigation of the admin panel.
+    |
+    | For detailed instructions you can look here:
+    |
+    */
+
+    'menu' => [
+        // Navbar items:
+        [
+            'text' => 'Home',
+            'icon' => 'ti ti-home',
+            'url' => 'home'
+        ],
+
+        [
+            'text' => 'Support 1',
+            'url' => '#',
+            'icon' => 'ti ti-help',
+            'active' => ['support1'],
+            'submenu' => [
+                [
+                    'text' => 'Ticket',
+                    'url' => 'support1',
+                    'icon' => 'ti ti-article',
+                ]
+            ],
+        ],
+
+        [
+            'text' => 'Support 2',
+            'url' => '#',
+            'icon' => 'ti ti-help',
+            'active' => ['support2'],
+            'submenu' => [
+                [
+                    'text' => 'Ticket',
+                    'url' => 'support2',
+                    'icon' => 'ti ti-article',
+                ]
+            ],
+        ],
+
+        [
+            'text' => 'Support 3',
+            'url' => '#',
+            'icon' => 'ti ti-help',
+            'active' => ['support3'],
+            'submenu' => [
+                [
+                    'text' => 'Ticket',
+                    'url' => 'support3',
+                    'icon' => 'ti ti-article',
+                ]
+            ],
+        ],
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Menu Filters
+    |--------------------------------------------------------------------------
+    |
+    | Here we can modify the menu filters of the admin panel.
+    |
+    | For detailed instructions you can look the menu filters section here:
+    |
+    */
+
+    'filters' => [
+        TakiElias\Tablar\Menu\Filters\GateFilter::class,
+        TakiElias\Tablar\Menu\Filters\HrefFilter::class,
+        TakiElias\Tablar\Menu\Filters\SearchFilter::class,
+        TakiElias\Tablar\Menu\Filters\ActiveFilter::class,
+        TakiElias\Tablar\Menu\Filters\ClassesFilter::class,
+        TakiElias\Tablar\Menu\Filters\LangFilter::class,
+        TakiElias\Tablar\Menu\Filters\DataFilter::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Vite
+    |--------------------------------------------------------------------------
+    |
+    | Here we can enable the Vite support.
+    |
+    | For detailed instructions you can look the Vite here:
+    | https://laravel-vite.dev
+    |
+    */
+
+    'vite' => true,
+];
